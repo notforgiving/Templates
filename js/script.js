f28e090baf4bd0ed3e9250a968f70c057289a5dd
Ludmila Ivleva <ludmila_web@leadrock.com>
2023-09-12 14:49:30 +0300

    add new template

--- js/script.js
+++ js/script.js
@@ -1,29 +1,34 @@
 const templates = [
-  "LRPROMO-1056/",
-  "LRPROMO-1057/",
-  "LRPROMO-1148/",
-  "LRPROMO-1151/",
-  "LRPROMO-1170/",
-  "LRPROMO-1252/",
-  "LRPROMO-1253/",
-  "/LRPROMO-1279/",
-  "LRPROMO-1329/",
-  "LRPROMO-1331/",
-  "LRPROMO-1339/1/",
-  "LRPROMO-1365/",
-  "LRPROMO-1387/",
-  "LRPROMO-1436/1/",
-  "LRPROMO-1467/",
-  "tretjakov.n/LRPROMO-1433/1/",
+  "LRPROMO-1056",
+  "LRPROMO-1057",
+  "LRPROMO-1148",
+  "LRPROMO-1151",
+  "LRPROMO-1170",
+  "LRPROMO-1252",
+  "LRPROMO-1253",
+  "LRPROMO-1279",
+  "LRPROMO-1329",
+  "LRPROMO-1331",
+  "LRPROMO-1339/1",
+  "LRPROMO-1365",
+  "LRPROMO-1368",
+  "LRPROMO-1387",
+  "LRPROMO-1396",
+  "LRPROMO-1429",
+  "LRPROMO-1436/1",
+  "LRPROMO-1460",
+  "LRPROMO-1467",
+  "tretjakov.n/LRPROMO-1433/1",
+  "LRPROMO-1516",
 ];
 
 const wrapper = document.querySelector("#container");
 
 const temp = templates.map((item) => {
   return `<iframe
-  src="http://v2dio.abbiamo.rotto.tutto.leadrocktest.com/${item}"
+  src="http://v2dio.abbiamo.rotto.tutto.leadrocktest.com/${item}/"
   frameborder="1"
-  width="480px"
+  width="520px"
   height="500px"
 ></iframe>`;
 });
