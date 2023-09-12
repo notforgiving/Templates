eec6fb4e525f8de32451bb7a89ec44f43b309936
Ludmila Ivleva <ludmila_web@leadrock.com>
2023-09-12 14:37:18 +0300

    ++

--- js/script.js
+++ js/script.js
@@ -1,22 +1,27 @@
 const templates = [
-    
-  "http://v2dio.abbiamo.rotto.tutto.leadrocktest.com/LRPROMO-1339/1/",
-  "http://v2dio.abbiamo.rotto.tutto.leadrocktest.com/LRPROMO-1148/",
-  "http://v2dio.abbiamo.rotto.tutto.leadrocktest.com/LRPROMO-1329/",
-  "http://v2dio.abbiamo.rotto.tutto.leadrocktest.com/LRPROMO-1331/",
-  "http://v2dio.abbiamo.rotto.tutto.leadrocktest.com/LRPROMO-1252/",
-  "http://v2dio.abbiamo.rotto.tutto.leadrocktest.com/LRPROMO-1253/",
-  "http://v2dio.abbiamo.rotto.tutto.leadrocktest.com/LRPROMO-1170/",
-  "http://v2dio.abbiamo.rotto.tutto.leadrocktest.com/LRPROMO-1151/",
-  "http://v2dio.abbiamo.rotto.tutto.leadrocktest.com/LRPROMO-1057/",
-  "http://v2dio.abbiamo.rotto.tutto.leadrocktest.com/LRPROMO-1056/",
-  "http://v2dio.abbiamo.rotto.tutto.leadrocktest.com/LRPROMO-1467/",
+  "LRPROMO-1056/",
+  "LRPROMO-1057/",
+  "LRPROMO-1148/",
+  "LRPROMO-1151/",
+  "LRPROMO-1170/",
+  "LRPROMO-1252/",
+  "LRPROMO-1253/",
+  "/LRPROMO-1279/",
+  "LRPROMO-1329/",
+  "LRPROMO-1331/",
+  "LRPROMO-1339/1/",
+  "LRPROMO-1365/",
+  "LRPROMO-1387/",
+  "LRPROMO-1436/1/",
+  "LRPROMO-1467/",
+  "tretjakov.n/LRPROMO-1433/1/",
 ];
+
 const wrapper = document.querySelector("#container");
 
 const temp = templates.map((item) => {
   return `<iframe
-  src="${item}"
+  src="http://v2dio.abbiamo.rotto.tutto.leadrocktest.com/${item}"
   frameborder="1"
   width="480px"
   height="500px"
